553f5bf71ed56baa4697339f5597993f2ca60478
Batuhan Yetgin <batuhan.yetgin@xinerji.com.tr>
2023-04-22 18:33:35 +0300

    Initial Commit

---
+++ RickAndMortyExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyExample
+//
+//  Created by Batuhan Yetgin on 22.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
